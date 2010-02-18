72fa50575aa1c6a9d1c2a1c2c2f54e5835516a6a
ronanburns <ronanburns@01531176-1851-496b-80bb-cb809b9bf42c>
2010-02-18 03:56:27 +0000

    turned chrome off due to a browser bug

--- trunk/tohu/tohu-clients/tohu-jquery-client/src/test/java/org/tohu/BaseTestFramework.java
+++ trunk/tohu/tohu-clients/tohu-jquery-client/src/test/java/org/tohu/BaseTestFramework.java
@@ -36,7 +36,7 @@ public class BaseTestFramework {
       ArrayList<Object []> list = new ArrayList<Object[]>();      
       list.add(new Object[]{BrowserType.IEXPLORER, "http://localhost:9999/unittest-web", "isGUIBusy"}); 
       list.add(new Object[]{BrowserType.FIREFOX, "http://localhost:9999/unittest-web", "isGUIBusy"});
-      list.add(new Object[]{BrowserType.CHROME, "http://localhost:9999/unittest-web", "isGUIBusy"});         
+      //list.add(new Object[]{BrowserType.CHROME, "http://localhost:9999/unittest-web", "isGUIBusy"});         
       //list.add(new Object[]{BrowserType.SAFARI, "http://localhost:9999/unittest-web", "isGUIBusy"});   
       return list; 
     }  
