b3b628db17c7d1ef77815a6285449c29577ad457
damonhorrell <damonhorrell@01531176-1851-496b-80bb-cb809b9bf42c>
2012-06-20 00:15:49 +0000

    fixed css (properly this time)

--- examples/loyalty-card-examples/loyalty-complex-example/loyalty-complex-example-web/src/main/webapp/script/extras.js
+++ examples/loyalty-card-examples/loyalty-complex-example/loyalty-complex-example-web/src/main/webapp/script/extras.js
@@ -1,5 +1,5 @@
 /*
-* Overriding because "requiredMissing" errors are hidden so we need to inform user that all fields are required. 
+* Overriding because "required" errors are hidden so we need to inform user that all fields are required. 
 */
 function notifyErrors() {
 	alert("Please answer all questions.");
